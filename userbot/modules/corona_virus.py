d87cc08396421d6e7249bab7aabc73726851ec33
MHMDFJRMLN <47522894+MuhamadFajarGaray@users.noreply.github.com>
2020-03-22 09:20:10 +0700

    Update corona_virus.py

--- userbot/modules/corona_virus.py
+++ userbot/modules/corona_virus.py
@@ -9,7 +9,7 @@ from covid import Covid
 from userbot import CMD_HELP
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.coronavirus(?: |$)(.*)")
+@register(outgoing=True, pattern="^.covid(?: |$)(.*)")
 async def corona(event):
     covid = Covid()
     data = covid.get_data()
